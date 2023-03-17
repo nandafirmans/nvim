f9b3eedadfed65e945503cd844b2839d6ebaf058
Nanda Firmansyah <nandafirmans@gmail.com>
2023-03-17 22:48:24 +0700

    add github copilot

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,28 @@
 return {
 
   {
+    "github/copilot.vim",
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+      vim.g.copilot_filetypes = {
+        ["*"] = false,
+        ["javascript"] = true,
+        ["typescript"] = true,
+        ["css"] = true,
+        ["html"] = true,
+        ["lua"] = false,
+        ["rust"] = false,
+        ["c"] = false,
+        ["c#"] = false,
+        ["c++"] = false,
+        ["go"] = true,
+        ["python"] = false,
+      }
+    end
+  },
+
+  {
     -- Autocompletion
     "hrsh7th/nvim-cmp",
     dependencies = {
