527b898ce449dc47a2407c43d1c5f50eaabd82a5
Nanda Firmansyah <nandafirmans@gmail.com>
2023-03-24 13:51:01 +0700

    chore: update dependencies nvim-lsp-file-operations

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -19,7 +19,10 @@ return {
   {
     "antosha417/nvim-lsp-file-operations",
     dependencies = {
-      { "nvim-lua/plenary.nvim" },
+      {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-tree.lua",
+      },
     },
     config = function()
       require("lsp-file-operations").setup()
