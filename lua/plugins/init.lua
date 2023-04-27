b219964d0ec1721021521723d0c257f674dd8079
Nanda Firmansyah <nandafirmans@gmail.com>
2023-04-27 08:49:11 +0700

    feat: install jackMort/ChatGPT.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,6 +25,19 @@ return {
     end
   },
 
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup()
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  },
+
   -- {
   --   "zbirenbaum/copilot.lua",
   --   cmd = "Copilot",
