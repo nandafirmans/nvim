8c1fd19071c8df433fcda7bcda7c5c6f769310df
Nanda Firmansyah <nandafirmans@gmail.com>
2023-04-27 08:49:39 +0700

    chore: remove autoclose buffer

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -251,28 +251,28 @@ return {
   },
 
   -- Auto close buffer on inactivity
-  {
-    "chrisgrieser/nvim-early-retirement",
-    event = "VeryLazy",
-    opts = {
-      -- if a buffer has been inactive for this many minutes, close it
-      retirementAgeMins = 20,
-      -- filetypes to ignore
-      ignoredFiletypes = {},
-      -- will not close the alternate file
-      ignoreAltFile = true,
-      -- will ignore buffers with unsaved changes. If false, the buffers will
-      -- automatically be written and then closed.
-      ignoreUnsavedChangesBufs = true,
-      -- ignore non-empty buftypes, e.g. terminal buffers
-      ignoreSpecialBuftypes = true,
-      -- uses vim.notify for plugins like nvim-notify
-      notificationOnAutoClose = false,
-    },
-    config = function(_, opts)
-      require("early-retirement").setup(opts)
-    end,
-  },
+  -- {
+  --   "chrisgrieser/nvim-early-retirement",
+  --   event = "VeryLazy",
+  --   opts = {
+  --     -- if a buffer has been inactive for this many minutes, close it
+  --     retirementAgeMins = 20,
+  --     -- filetypes to ignore
+  --     ignoredFiletypes = {},
+  --     -- will not close the alternate file
+  --     ignoreAltFile = true,
+  --     -- will ignore buffers with unsaved changes. If false, the buffers will
+  --     -- automatically be written and then closed.
+  --     ignoreUnsavedChangesBufs = true,
+  --     -- ignore non-empty buftypes, e.g. terminal buffers
+  --     ignoreSpecialBuftypes = true,
+  --     -- uses vim.notify for plugins like nvim-notify
+  --     notificationOnAutoClose = false,
+  --   },
+  --   config = function(_, opts)
+  --     require("early-retirement").setup(opts)
+  --   end,
+  -- },
 
   -- Multi Cursor
   {
