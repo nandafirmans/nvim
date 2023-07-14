b07f10a5876f0fa27b7ad9fd4b15e117e58b8414
nandafirmans <nandafirmans@gmail.com>
2023-07-14 09:43:17 +0700

    update

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,168 @@
+return {
+  -- Fuzzy Finder (files, lsp, etc)
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim", },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-u>"] = false,
+              ["<C-d>"] = false,
+            },
+          },
+          path_display = {
+            truncate = 3
+          }
+        },
+        pickers = {
+          colorscheme = {
+            theme = "dropdown",
+            enable_preview = true,
+          },
+          buffers = {
+            theme = "dropdown",
+            previewer = false,
+            path_display = {
+              shorten = 2,
+              truncate = 3,
+            }
+          },
+        },
+        extensions = {
+          file_browser = {
+            theme = "dropdown",
+            hijack_netrw = true,
+            previewer = false,
+          },
+        },
+      })
+
+      require("telescope").load_extension("fzf")
+      require("telescope").load_extension("file_browser")
+      require("telescope").load_extension("flutter")
+      require("telescope").load_extension("session-lens")
+    end,
+    keys = {
+      {
+        "<leader>?",
+        function() require("telescope.builtin").oldfiles(); end,
+        desc = "[?] Find recently opened files"
+      },
+      {
+        "<leader><space>",
+        function() require("telescope.builtin").buffers(); end,
+        desc = "[ ] Find existing buffers"
+      },
+      {
+        "<leader>f/",
+        function()
+          require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+            winblend = 10,
+            previewer = false,
+          }));
+        end,
+        desc = "[/] Fuzzily search in current buffer]"
+      },
+      {
+        "gr",
+        function() require("telescope.builtin").lsp_references(); end,
+        desc =
+        "[ ] LSP References"
+      },
+      {
+        "<leader>ds",
+        function() require("telescope.builtin").lsp_document_symbols(); end,
+        desc =
+        "[D]ocument [S]ymbols"
+      },
+      {
+        "<leader>ws",
+        function() require("telescope.builtin").lsp_dynamic_workspace_symbols(); end,
+        desc =
+        "[W]orkspace [S]ymbols"
+      },
+      {
+        "<leader>fr",
+        function() require("telescope.builtin").resume(); end,
+        desc =
+        "[ ] Resume Last Find"
+      },
+      {
+        "<leader>ff",
+        function() require("telescope.builtin").find_files(); end,
+        desc =
+        "[F]ind [F]iles"
+      },
+      {
+        "<leader>fh",
+        function() require("telescope.builtin").help_tags(); end,
+        desc =
+        "[F]ind [H]elp"
+      },
+      {
+        "<leader>fw",
+        function() require("telescope.builtin").grep_string(); end,
+        desc =
+        "[F]ind current [W]ord"
+      },
+      {
+        "<leader>fg",
+        function() require("telescope.builtin").live_grep(); end,
+        desc =
+        "[F]ind by [G]rep"
+      },
+      {
+        "<leader>fd",
+        function() require("telescope.builtin").diagnostics(); end,
+        desc =
+        "[F]ind [D]iagnostics"
+      },
+      {
+        "<leader>fc",
+        function() require("telescope.builtin").commands(); end,
+        desc =
+        "[F]ind [C]ommands"
+      },
+      {
+        "<leader>fs",
+        function() require("telescope.builtin").colorscheme(); end,
+        desc =
+        "[F]ind color[S]chemes"
+      },
+      {
+        "<leader>fk",
+        function() require("telescope.builtin").keymaps(); end,
+        desc =
+        "[F]ind [K]eymaps"
+      },
+      {
+        "<leader>ft",
+        "<Cmd>TodoTelescope<CR>",
+        desc =
+        "[F]ind [T]odo"
+      },
+      {
+        "<leader>fb",
+        "<Cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>",
+        desc =
+        "[F]ile [B]rowser"
+      },
+    }
+  },
+
+  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    cond = vim.fn.executable("make") == 1
+  },
+
+  -- Telescope File Browser
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", },
+  },
+}
