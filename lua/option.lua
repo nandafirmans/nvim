e8fef29ac4af65d2e5e299663abd75422ef2972f
nandafirmans <nandafirmans@gmail.com>
2023-08-14 21:49:15 +0700

    update theme

--- lua/option.lua
+++ lua/option.lua
@@ -54,7 +54,7 @@ vim.o.updatetime = 250
 vim.wo.signcolumn = "yes"
 
 -- Set colorscheme
-vim.cmd([[colorscheme sonokai]])
+vim.cmd([[colorscheme molokai]])
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = "menuone,noselect"
