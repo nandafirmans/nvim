d67324b1e1b51284c439db278c2a68c770651fcf
nandafirmans <nandafirmans@gmail.com>
2023-08-18 13:52:50 +0700

    feat: copilot cmp

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -1,55 +1,49 @@
 return {
 
-  {
-    "github/copilot.vim",
-    lazy = false,
-    config = function()
-      vim.api.nvim_set_keymap("i", "<A-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-      vim.g.copilot_no_tab_map = true
-      vim.g.copilot_filetypes = {
-        ["*"] = false,
-        ["javascript"] = true,
-        ["typescript"] = true,
-        ["css"] = true,
-        ["scss"] = true,
-        ["less"] = true,
-        ["html"] = true,
-        ["lua"] = true,
-        ["rust"] = false,
-        ["c"] = false,
-        ["c#"] = false,
-        ["c++"] = false,
-        ["go"] = true,
-        ["python"] = false,
-      }
-    end
-  },
-
   -- {
-  --   "zbirenbaum/copilot.lua",
-  --   cmd = "Copilot",
-  --   event = "InsertEnter",
+  --   "github/copilot.vim",
+  --   lazy = false,
   --   config = function()
-  --     require("copilot").setup({
-  --       suggestion = { enabled = false },
-  --       panel = { enabled = false },
-  --     })
-  --   end,
-  -- },
-  --
-  -- {
-  --   "zbirenbaum/copilot-cmp",
-  --   dependencies = { "copilot.lua" },
-  --   config = function()
-  --     require("copilot_cmp").setup({
-  --       formatters = {
-  --         label = require("copilot_cmp.format").format_label_text,
-  --         insert_text = require("copilot_cmp.format").format_insert_text,
-  --         preview = require("copilot_cmp.format").deindent,
-  --       },
-  --     })
+  --     vim.api.nvim_set_keymap("i", "<A-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+  --     vim.g.copilot_no_tab_map = true
+  --     vim.g.copilot_filetypes = {
+  --       ["*"] = false,
+  --       ["javascript"] = true,
+  --       ["typescript"] = true,
+  --       ["css"] = true,
+  --       ["scss"] = true,
+  --       ["less"] = true,
+  --       ["html"] = true,
+  --       ["lua"] = true,
+  --       ["rust"] = false,
+  --       ["c"] = false,
+  --       ["c#"] = false,
+  --       ["c++"] = false,
+  --       ["go"] = true,
+  --       ["python"] = false,
+  --     }
   --   end
   -- },
+
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end,
+  },
+
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = { "copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end
+  },
 
   {
     -- Autocompletion
@@ -65,11 +59,11 @@ return {
       "rafamadriz/friendly-snippets",
     },
     config = function()
-      -- local has_words_before = function()
-      --   if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-      --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      --   return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-      -- end
+      local has_words_before = function()
+        if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+      end
 
       -- nvim-cmp setup
       local cmp = require("cmp")
@@ -90,6 +84,7 @@ return {
           format = require('lspkind').cmp_format({
             mode = 'symbol',
             maxwidth = 50,
+            symbol_map = { Copilot = "" }
           }),
         },
         mapping = cmp.mapping.preset.insert({
@@ -101,9 +96,10 @@ return {
             select = true,
           }),
           ["<Tab>"] = cmp.mapping(function(fallback)
-            -- if cmp.visible() and has_words_before() then
-            if cmp.visible() then
-              cmp.select_next_item()
+            if cmp.visible() and has_words_before() then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+              -- if cmp.visible() then
+              -- cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
               luasnip.expand_or_jump()
             else
@@ -121,7 +117,7 @@ return {
           end, { "i", "s" }),
         }),
         sources = {
-          -- { name = "copilot",  group_index = 2 },
+          { name = "copilot",  group_index = 2 },
           { name = "nvim_lsp", group_index = 2 },
           { name = "luasnip",  group_index = 2 },
           { name = "path",     group_index = 2 },
