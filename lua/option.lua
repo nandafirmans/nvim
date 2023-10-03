64313536932e31918612e864e33929926dc3186f
Nanda Firmansyah <nandafirmans@gmail.com>
2023-10-03 11:11:10 +0700

    theme

--- lua/option.lua
+++ lua/option.lua
@@ -54,7 +54,7 @@ vim.o.updatetime = 250
 vim.wo.signcolumn = "yes"
 
 -- Set colorscheme
-vim.cmd([[colorscheme monokai-pro]])
+vim.cmd([[colorscheme kanagawa]])
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = "menuone,noselect"
