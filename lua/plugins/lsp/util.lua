1774abbe40a3d1cbb74b306e673f8ec5ea0064de
Nanda Firmansyah <nandafirmans@gmail.com>
2023-10-10 22:13:50 +0700

    fix: go to definition

--- lua/plugins/lsp/util.lua
+++ lua/plugins/lsp/util.lua
@@ -29,7 +29,7 @@ M.on_attach = function(_, bufnr)
 
   -- nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
   -- nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-  -- nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+  nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
   nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
   nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
 
