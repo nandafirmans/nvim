942104f9dbe26e29188aa7ec7bf8caedca59f52b
Nanda Firmansyah <nandafirmans@gmail.com>
2024-03-18 14:33:00 +0700

    install nvim spectre

--- lua/plugins/editor_ui.lua
+++ lua/plugins/editor_ui.lua
@@ -139,4 +139,35 @@ return {
       require("bufferline").setup(opts)
     end
   },
+
+  {
+    "nvim-pack/nvim-spectre",
+    lazy = false,
+    dependencies = "nvim-lua/plenary.nvim",
+    keys = {
+      {
+        "<leader>F",
+        '<CMD>lua require("spectre").toggle()<CR>',
+        mode = "n",
+        desc = "Toggle Spectre"
+      },
+      {
+        "<leader>F",
+        '<ESC><CMD>lua require("spectre").open_visual()<CR>',
+        mode = "v",
+        desc = "Search Selected Text"
+      },
+      {
+        "<leader>FF",
+        '<CMD>lua require("spectre").open_file_search({select_word=true})<CR>',
+        mode = "n",
+        desc = "Search on current file"
+      }
+    },
+    config = function()
+      require('spectre').setup({
+        is_block_ui_break = true
+      })
+    end
+  }
 }
