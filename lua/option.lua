cec1b35327579948f18537c66699b9d3d92f5c74
Nanda Firmansyah <nandafirmans@gmail.com>
2024-04-06 22:53:43 +0700

    github theme

--- lua/option.lua
+++ lua/option.lua
@@ -60,7 +60,7 @@ vim.o.updatetime = 250
 vim.wo.signcolumn = "yes"
 
 -- Set colorscheme
-vim.cmd([[colorscheme nightfly]])
+vim.cmd([[colorscheme github_dark_dimmed]])
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = "menuone,noselect"
