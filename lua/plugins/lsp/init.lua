35da3dd29fe1db93d0119c30db35cde002139c32
nandafirmans <nandafirmans@gmail.com>
2025-02-03 17:26:41 +0700

    fix: prettier format

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -69,7 +69,7 @@ return {
       local null_ls = require("null-ls")
       null_ls.setup({
         sources = {
-          null_ls.builtins.formatting.prettierd,
+          null_ls.builtins.formatting.prettier,
           null_ls.builtins.formatting.sqlfluff,
           null_ls.builtins.formatting.gofmt,
           null_ls.builtins.formatting.goimports,
