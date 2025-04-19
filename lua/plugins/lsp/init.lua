3f689fcc8f0b8524712c93dd1eed6ea33b3eef9e
nandafirmans <nandafirmans@gmail.com>
2025-04-19 11:46:21 +0700

    fix(lsp): roslyn

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -270,18 +270,13 @@ return {
       local capabilities = lsp_util.capabilities;
       local on_attach = lsp_util.on_attach;
 
-      require("roslyn").setup({
+      require('roslyn').setup {
         args = {
+          '--stdio',
           '--logLevel=Information',
           '--extensionLogDirectory=' .. vim.fs.dirname(vim.lsp.get_log_path()),
-          '--razorSourceGenerator=' .. vim.fs.joinpath(
-            vim.fn.stdpath 'data' --[[@as string]],
-            'mason',
-            'packages',
-            'roslyn',
-            'libexec',
-            'Microsoft.CodeAnalysis.Razor.Compiler.dll'
-          ),
+          '--razorSourceGenerator='
+          .. vim.fs.joinpath(vim.fn.stdpath 'data' --[[@as string]], 'mason', 'packages', 'roslyn', 'libexec', 'Microsoft.CodeAnalysis.Razor.Compiler.dll'),
           '--razorDesignTimePath=' .. vim.fs.joinpath(
             vim.fn.stdpath 'data' --[[@as string]],
             'mason',
@@ -296,28 +291,17 @@ return {
           on_attach = on_attach,
           capabilities = capabilities,
           handlers = require('rzls.roslyn_handlers'),
-          -- settings = {
-          --   ['csharp|inlay_hints'] = {
-          --     csharp_enable_inlay_hints_for_implicit_object_creation = true,
-          --     csharp_enable_inlay_hints_for_implicit_variable_types = true,
-          --
-          --     csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-          --     csharp_enable_inlay_hints_for_types = true,
-          --     dotnet_enable_inlay_hints_for_indexer_parameters = true,
-          --     dotnet_enable_inlay_hints_for_literal_parameters = true,
-          --     dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-          --     dotnet_enable_inlay_hints_for_other_parameters = true,
-          --     dotnet_enable_inlay_hints_for_parameters = true,
-          --     dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-          --     dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-          --     dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
-          --   },
-          --   ['csharp|code_lens'] = {
-          --     dotnet_enable_references_code_lens = true,
-          --   },
-          -- },
         },
-      })
-    end
+      }
+    end,
+    init = function()
+      -- we add the razor filetypes before the plugin loads
+      vim.filetype.add {
+        extension = {
+          razor = 'razor',
+          cshtml = 'razor',
+        },
+      }
+    end,
   }
 }
