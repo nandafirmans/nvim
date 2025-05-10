21d2876ed89112ae396b02eceae79cec1595ae39
nandafirmans <nandafirmans@gmail.com>
2025-05-10 14:22:26 +0700

    update

--- lua/option.lua
+++ lua/option.lua
@@ -65,7 +65,7 @@ vim.wo.signcolumn = "yes"
 -- vim.cmd([[colorschem ayu-mirage]])
 -- vim.cmd([[colorschem catppgruvboxuccin]])
 -- vim.cmd([[colorschem bluloco]])
-vim.cmd([[colorschem nightfly]])
+vim.cmd([[colorschem monokai-pro-spectrum]])
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = "menuone,noselect"
