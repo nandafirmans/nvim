5f85d37a5647ceac9dd4f91d2a897f698ccb9203
nandafirmans <nandafirmans@gmail.com>
2025-07-30 10:13:06 +0700

    update

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -22,11 +22,14 @@ return {
       -- nvim-cmp setup
       local cmp = require("cmp")
       local luasnip = require("luasnip")
+      local lspkind = require("lspkind")
 
       -- LuaSnip
       require("luasnip.loaders.from_vscode").lazy_load()
       luasnip.filetype_extend("javascript", { "javascriptreact" })
       luasnip.filetype_extend("javascript", { "html" })
+
+
 
       cmp.setup({
         snippet = {
@@ -34,13 +37,35 @@ return {
             luasnip.lsp_expand(args.body)
           end,
         },
-        formatting = {
-          format = require('lspkind').cmp_format({
-            mode = 'symbol',
-            maxwidth = 50,
-            symbol_map = { Copilot = "", minuet = "M", },
-          }),
+        window = {
+          completion = {
+            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+            col_offset = -3,
+            side_padding = 0,
+          },
         },
+        formatting = {
+          fields = { "kind", "abbr", "menu" },
+          format = function(entry, vim_item)
+            local kind = lspkind.cmp_format({
+              mode = "symbol_text",
+              maxwidth = 50,
+              symbol_map = { Copilot = "", minuet = "M", },
+            })(entry, vim_item)
+            local strings = vim.split(kind.kind, "%s", { trimempty = true })
+            kind.kind = " " .. (strings[1] or "") .. " "
+            kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+            return kind
+          end,
+        },
+        -- formatting = {
+        --   format = lspkind.cmp_format({
+        --     mode = 'symbol',
+        --     maxwidth = 50,
+        --     symbol_map = { Copilot = "", minuet = "M", },
+        --   }),
+        -- },
         performance = {
           fetching_timeout = 2000,
         },
