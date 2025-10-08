35162dbdfc56f2420b971805ea55d6f76698ade4
nandafirmans <nandafirmans@gmail.com>
2025-10-08 09:31:35 +0700

    feat: install sidekick

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -130,6 +130,70 @@ return {
     end,
   },
   {
+    "folke/sidekick.nvim",
+    opts = {
+      cli = {
+        mux = {
+          enabled = true,
+          backend = "tmux",
+        },
+      },
+    },
+    keys = {
+      {
+        "<tab>",
+        function()
+          -- if there is a next edit, jump to it, otherwise apply it if any
+          if not require("sidekick").nes_jump_or_apply() then
+            return "<Tab>" -- fallback to normal tab
+          end
+        end,
+        expr = true,
+        desc = "Goto/Apply Next Edit Suggestion",
+      },
+      -- {
+      --   "<leader>aa",
+      --   function() require("sidekick.cli").toggle() end,
+      --   desc = "Sidekick Toggle CLI",
+      -- },
+      {
+        "<leader>as",
+        function() require("sidekick.cli").select({ filter = { installed = true } }) end,
+        desc = "Select CLI",
+      },
+      {
+        "<leader>at",
+        function() require("sidekick.cli").send({ msg = "{this}" }) end,
+        mode = { "x", "n" },
+        desc = "Send This",
+      },
+      {
+        "<leader>av",
+        function() require("sidekick.cli").send({ msg = "{selection}" }) end,
+        mode = { "x" },
+        desc = "Send Visual Selection",
+      },
+      {
+        "<leader>ap",
+        function() require("sidekick.cli").prompt() end,
+        mode = { "n", "x" },
+        desc = "Sidekick Select Prompt",
+      },
+      {
+        "<c-.>",
+        function() require("sidekick.cli").focus() end,
+        mode = { "n", "x", "i", "t" },
+        desc = "Sidekick Switch Focus",
+      },
+      {
+        "<leader>ac",
+        function() require("sidekick.cli").toggle({ name = "copilot", focus = true }) end,
+        mode = { "n", "x", "i", "t" },
+        desc = "Sidekick Toggle Opencode",
+      },
+    },
+  },
+  {
     "yetone/avante.nvim",
     event = "VeryLazy",
     lazy = false,
