24b8ddc1d8d02e75f0ae0bcc56a0464ac5b8e05e
nandafirmans <nandafirmans@gmail.com>
2025-10-09 09:47:12 +0700

    update

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -143,22 +143,6 @@ return {
       }
     },
     keys = {
-      -- {
-      --   "<tab>",
-      --   function()
-      --     -- if there is a next edit, jump to it, otherwise apply it if any
-      --     if not require("sidekick").nes_jump_or_apply() then
-      --       return "<Tab>" -- fallback to normal tab
-      --     end
-      --   end,
-      --   expr = true,
-      --   desc = "Goto/Apply Next Edit Suggestion",
-      -- },
-      -- {
-      --   "<leader>aa",
-      --   function() require("sidekick.cli").toggle() end,
-      --   desc = "Sidekick Toggle CLI",
-      -- },
       {
         "<leader>as",
         function() require("sidekick.cli").select({ filter = { installed = true } }) end,
@@ -166,13 +150,13 @@ return {
       },
       {
         "<leader>at",
-        function() require("sidekick.cli").send({ msg = "{this}" }) end,
+        function() require("sidekick.cli").send({ msg = "{this}", filter = { installed = true } }) end,
         mode = { "x", "n" },
         desc = "Send This",
       },
       {
         "<leader>av",
-        function() require("sidekick.cli").send({ msg = "{selection}" }) end,
+        function() require("sidekick.cli").send({ msg = "{selection}", filter = { installed = true } }) end,
         mode = { "x" },
         desc = "Send Visual Selection",
       },
@@ -183,15 +167,9 @@ return {
         desc = "Sidekick Select Prompt",
       },
       {
-        "<c-.>",
-        function() require("sidekick.cli").focus() end,
-        mode = { "n", "x", "i", "t" },
-        desc = "Sidekick Switch Focus",
-      },
-      {
-        "<leader>ac",
+        "<leader>ax",
         function() require("sidekick.cli").toggle({ name = "copilot", focus = true }) end,
-        mode = { "n", "x", "i", "t" },
+        mode = { "n", "x", "t" },
         desc = "Sidekick Toggle Opencode",
       },
     },
