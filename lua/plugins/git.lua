c458806d982381715e4f3d567360c829d2bc99df
nandafirmans <nandafirmans@gmail.com>
2025-11-17 17:21:21 +0700

    update

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,47 +1,47 @@
 return {
-  { "tpope/vim-fugitive" },
-  { "tpope/vim-rhubarb" },
+	{ "tpope/vim-fugitive" },
+	{ "tpope/vim-rhubarb" },
 
-  {
-    "lewis6991/gitsigns.nvim",
-    lazy = false,
-    keys = {
-      {
-        "<leader>gb",
-        "<Cmd>Gitsigns toggle_current_line_blame<CR>",
-        mode = "n",
-      },
-    },
-    config = function()
-      require("gitsigns").setup({
-        signs = {
-          add = { text = "+" },
-          change = { text = "~" },
-          delete = { text = "_" },
-          topdelete = { text = "‾" },
-          changedelete = { text = "~" },
-        },
-      });
+	{
+		"lewis6991/gitsigns.nvim",
+		lazy = false,
+		keys = {
+			{
+				"<leader>gb",
+				"<Cmd>Gitsigns toggle_current_line_blame<CR>",
+				mode = "n",
+			},
+		},
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+					add = { text = "+" },
+					change = { text = "~" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+				},
+			})
 
-      vim.cmd([[ Gitsigns toggle_current_line_blame ]]);
-    end
-  },
+			vim.cmd([[ Gitsigns toggle_current_line_blame ]])
+		end,
+	},
 
-  {
-    "sindrets/diffview.nvim",
-    dependencies = "nvim-lua/plenary.nvim",
-    keys = {
-      { "<leader>dd", "<Cmd>DiffviewOpen<CR>",          mode = "n", },
-      { "<leader>dr", "<Cmd>DiffviewRefresh<CR>",       mode = "n", },
-      { "<leader>df", "<Cmd>DiffviewFileHistory %<CR>", mode = "n", },
-      { "<leader>dh", "<Cmd>DiffviewFileHistory<CR>",   mode = "n", },
-      { "<leader>dc", "<Cmd>DiffviewClose<CR>",         mode = "n", },
-    }
-  },
+	{
+		"sindrets/diffview.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+		keys = {
+			{ "<leader>dd", "<Cmd>DiffviewOpen<CR>", mode = "n" },
+			{ "<leader>dr", "<Cmd>DiffviewRefresh<CR>", mode = "n" },
+			{ "<leader>df", "<Cmd>DiffviewFileHistory % --first-parent<CR>", mode = "n" },
+			{ "<leader>dh", "<Cmd>DiffviewFileHistory --first-parent<CR>", mode = "n" },
+			{ "<leader>dc", "<Cmd>DiffviewClose<CR>", mode = "n" },
+		},
+	},
 
-  {
-    'akinsho/git-conflict.nvim',
-    version = "*",
-    config = true,
-  },
+	{
+		"akinsho/git-conflict.nvim",
+		version = "*",
+		config = true,
+	},
 }
