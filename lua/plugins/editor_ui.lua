81ee4d20fe631246e387f70dbd5f6b3d84e63a77
nandafirmans <nandafirmans@gmail.com>
2025-12-05 09:20:03 +0700

    update

--- lua/plugins/editor_ui.lua
+++ lua/plugins/editor_ui.lua
@@ -284,13 +284,34 @@ return {
 		config = function()
 			local animate = require("mini.animate")
 
+			-- don't use animate when scrolling with the mouse
+			local mouse_scrolled = false
+			for _, scroll in ipairs({ "Up", "Down" }) do
+				local key = "<ScrollWheel" .. scroll .. ">"
+				vim.keymap.set({ "", "i" }, key, function()
+					mouse_scrolled = true
+					return key
+				end, { expr = true })
+			end
+
 			animate.setup({
 				cursor = {
 					enable = false,
 				},
-				scroll = {
-					enable = true,
+				resize = {
 					timing = animate.gen_timing.linear({ duration = 50, unit = "total" }),
+				},
+				scroll = {
+					timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+					subscroll = animate.gen_subscroll.equal({
+						predicate = function(total_scroll)
+							if mouse_scrolled then
+								mouse_scrolled = false
+								return false
+							end
+							return total_scroll > 1
+						end,
+					}),
 				},
 			})
 		end,
