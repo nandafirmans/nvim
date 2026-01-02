5cac26ca08f0193ac55bbb02769d84263a1c43f7
nandafirmans <nandafirmans@gmail.com>
2026-01-02 09:13:14 +0700

    update

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,6 +1,12 @@
 return {
 	{
 		"zbirenbaum/copilot.lua",
+		requires = {
+			"copilotlsp-nvim/copilot-lsp",
+			init = function()
+				vim.g.copilot_nes_debounce = 500
+			end,
+		},
 		cmd = "Copilot",
 		event = "InsertEnter",
 		config = function()
--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -93,11 +93,28 @@ return {
 					end, { "i", "s" }),
 				}),
 				sources = {
-					{ name = "minuet", group_index = 2 },
 					{ name = "copilot", group_index = 2 },
 					{ name = "nvim_lsp", group_index = 2 },
 					{ name = "luasnip", group_index = 2 },
 					{ name = "path", group_index = 2 },
+				},
+				sorting = {
+					priority_weight = 2,
+					comparators = {
+						require("copilot_cmp.comparators").prioritize,
+
+						-- Below is the default comparitor list and order for nvim-cmp
+						cmp.config.compare.offset,
+						-- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+						cmp.config.compare.exact,
+						cmp.config.compare.score,
+						cmp.config.compare.recently_used,
+						cmp.config.compare.locality,
+						cmp.config.compare.kind,
+						cmp.config.compare.sort_text,
+						cmp.config.compare.length,
+						cmp.config.compare.order,
+					},
 				},
 			})
 		end,
