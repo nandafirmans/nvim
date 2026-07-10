c13e814126a54c500a04efabd45ff367cf4cce75
nandafirmans <nandafirmans@gmail.com>
2026-07-10 13:59:58 +0700

    update

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -16,7 +16,7 @@ return {
 			},
 		},
 		config = function()
-			vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+			vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,localoptions"
 
 			local function close_alpha_if_open()
 				vim.cmd([[
@@ -26,11 +26,19 @@ return {
         ]])
 			end
 
+			local function close_toggleterm_buffers()
+				for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+					if vim.bo[bufnr].filetype == "toggleterm" then
+						vim.api.nvim_buf_delete(bufnr, { force = true })
+					end
+				end
+			end
+
 			require("auto-session").setup({
 				-- Automatically restore the session for the current working directory.
 				auto_restore = true,
 				suppressed_dirs = { "~/", "/" },
-				pre_save_cmds = { close_alpha_if_open, "tabdo NvimTreeClose", "tabdo DiffviewClose" },
+				pre_save_cmds = { close_alpha_if_open, close_toggleterm_buffers, "tabdo NvimTreeClose", "tabdo DiffviewClose" },
 			})
 		end,
 	},
